4b779f9d27ad642e0aab203692a600022a6b160b
Aldo FWI <aldo.fwi@gmail.com>
2023-11-03 16:58:30 -0400

    Logout Btn 🚸

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useAuthContext } from '@/context/AuthContext';
 import { useLogout } from '../hooks/useLogout';
 import Image from 'next/image';
+import Navbar from '@/pages/Navbar';
 
 const Home = () => {
 
@@ -15,34 +16,11 @@ const Home = () => {
 
   return (
     <div className="utility__page">
+        <Navbar />
 
             <div className="h-screen flex flex-col bg-[#121212] container justify-center items-center">
-                <h1 className="text-white font-mono font-bold">Hello World !</h1>
+                <h1 className="text-white font-mono font-bold">Hello World!</h1>
 
-                {user && (
-                    <>
-                    <div className="user">
-                        <p>You&apos;re logged in as :</p>
-    
-                        <span className='text-blue-300'>{ user.displayName }</span>
-                        <div className='w-20 h-20'>
-                            <Image
-                                loader={myLoader}
-                                unoptimized
-                                src={ user.photoURL } 
-                                className="profile_img"
-                                width={1}
-                                height={1}
-                                alt="pp" />
-                        </div>
-                    </div>
-                    <button className="logout bg-[#d05454] hover:bg-[#d05454]/90" onClick={logout}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9" />
-                        </svg>
-                    </button>
-                    </>
-                )}
             </div>
 
     </div>
---
+++ pages/Navbar.jsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React, { useState } from 'react';
+import { useAuthContext } from '@/context/AuthContext';
+import { useLogout } from '../hooks/useLogout';
+import Link from 'next/link';
+import Image from 'next/image';
+import ALogo from '/public/images/logoA_trans.png';
+
+const Navbar = () => {
+
+    const { user } = useAuthContext();
+    const { logout } = useLogout();
+    const myLoader = ({ src }) => {
+        return user.photoURL;
+    }
+    
+  return (
+        
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+        <div className="flex container lg:px-24 lg:py-4 flex-wrap items-center justify-between mx-auto px-24 py-2">
+            
+            <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
+                <Image src={ALogo} alt="Aldo Icon" height={50} width={50} />
+            </Link>
+
+            <div>
+                <ul className="font-[Stanley] flex">
+                    <li className='px-6'>
+                        <button className="text-blue-500 hover:text-white font-bold">Rules</button>
+                    </li>
+                    <li className='px-6'>
+                        <button className="text-blue-500 hover:text-white font-bold">Scored</button>
+                    </li>
+                    <li className='px-6'>
+                        <button className="text-blue-500 hover:text-white font-bold">Online</button>
+                    </li>
+                    <li className='px-6'>
+                        <Image
+                            loader={myLoader}
+                            unoptimized
+                            src={ user.photoURL } 
+                            className="profile_img"
+                            objectFit='fill'
+                            width="10"
+                            height="10"
+                            alt="pp" />
+                    </li>
+                    <li class="px-6 py-3">
+                        <button 
+                            type="button"
+                            className="px-5 py-4 font-medium rounded-full text-center text-white inline-flex items-center bg-[#d05454] hover:bg-[#d05454]/90" 
+                            data-tooltip-target="tooltip-bottom" 
+                            data-tooltip-placement="bottom"
+                            onClick={logout}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9" />
+                            </svg>
+                        </button>
+                    </li>
+                    
+                </ul>
+            </div>
+
+        </div>                   
+    </nav>
+
+  )
+}
+
+export default Navbar;
+
+/*
+
+                        <Image
+                            loader={myLoader}
+                            unoptimized
+                            src={ user.photoURL } 
+                            className="profile_img"
+                            width={1}
+                            height={1}
+                            alt="pp" />
+
+
+                        <img 
+                            src={user.photoURL} 
+                            alt="pp" />
+*/

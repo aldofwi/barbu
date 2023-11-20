ebc658c20a974f08c2f027b6f0586b5dd78231de
Aldo FWI <aldo.fwi@gmail.com>
2023-11-20 19:14:17 -0400

    Server try One

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const ENDPOINT = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000/';
+const ENDPOINT = process.env.NODE_ENV === 'production' ? 'http://barbu-server.vercel.app' : 'http://localhost:3000/';
 
 export const barbuWS = io(ENDPOINT); 
 
